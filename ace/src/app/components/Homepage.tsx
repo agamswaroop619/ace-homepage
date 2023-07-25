a7f4341974a133bace3bbba3d037d17af902dd3c
agamswaroop619 <agam.swaroop619@gmail.com>
2023-07-25 16:17:28 +0530

    homepage update

--- ace/src/app/components/Homepage.tsx
+++ ace/src/app/components/Homepage.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 // Homepage component
 export default function Homepage() {
   return (
-    <div className="h-[400vh] w-full relative top-20">
-      <section className="h-[100vh] bg-white w-full text-black">Homepage Section 1</section>
-      <section className="h-[100vh] bg-blue-500 w-full">Homepage Section 2</section>
-      <section className="h-[100vh] bg-white w-full text-black">Homepage Section 3</section>
-      <section className="h-[100vh] bg-blue-500 w-full">Homepage Section 4</section>
+    <div className="h-[400vh] w-full relative top-0 ">
+      <section className="h-[100vh] bg-white w-full text-black top-20 relative">Homepage Section 1</section>
+      <section className="h-[100vh] bg-blue-500 w-full top-20 relative">Homepage Section 2</section>
+      <section className="h-[100vh] bg-white w-full text-black top-20 relative">Homepage Section 3</section>
+      <section className="h-[100vh] bg-blue-500 w-full top-20 relative">Homepage Section 4</section>
     </div>
   );
 }
